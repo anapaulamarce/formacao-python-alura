d6ae5ff552ed2e3fbbf93f523d6c612222b48a70
anapaulamarce <anapmarcelino98@gmail.com>
2024-05-20 09:25:43 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+print ("""
+
+█▀▀ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 █▀▀ █─█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
+▀▀█ █▄▄█ █▀▀▄ █──█ █▄▄▀ 　 █▀▀ ▄▀▄ █──█ █▄▄▀ █▀▀ ▀▀█ ▀▀█ 
+▀▀▀ ▀──▀ ▀▀▀─ ▀▀▀▀ ▀─▀▀ 　 ▀▀▀ ▀─▀ █▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀ ▀▀▀
+""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opçao: ')
+print('Voce escolheu a opçao', opcao_escolhida)
